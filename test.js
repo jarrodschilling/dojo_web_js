4a098a1e98c66e7b5c8dc8013d80d94da6c1ed9f
Jarrod Schilling <jarrodschilling@Jarrods-MacBook-Air.local>
2023-09-27 14:11:11 -0600

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+function counter() {    
+        for (var num = 0; num <= 5; num++) {        
+            console.log(num);    
+        }
+    }
+    counter();    // run the function
+    counter();    // run the function again
+    
