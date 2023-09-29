87e04a606026c1081e47a9ab98b20325ee2bbf83
Jarrod Schilling <jarrodschilling@Jarrods-MacBook-Air.local>
2023-09-28 20:56:32 -0600

    likes Part 2 almost done

---
+++ Schilling_Jarrod_LikesPart2/script.js
@@ -0,0 +1,26 @@
+var neilCount = 0;
+var neilElement = document.querySelector("#neilCounter");
+
+
+function neilAdd() {
+    neilCount++;
+    neilElement.innerText = neilCount + " like(s)"
+}
+
+var nicholeCount = 0;
+var nicholeElement = document.querySelector("#nicholeCounter");
+
+
+function nicholeAdd() {
+    nicholeCount++;
+    nicholeElement.innerText = nicholeCount + " like(s)"
+}
+
+var jimCount = 0;
+var jimElement = document.querySelector("#jimCounter");
+
+
+function jimAdd() {
+    jimCount++;
+    jimElement.innerText = jimCount + " like(s)"
+}
